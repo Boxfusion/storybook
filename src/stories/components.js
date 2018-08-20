ee0af8884e9849bbc5171f8af5d3138e39e5557d
Mazi Muhlari <mazi.muhlari@gmail.com>
2018-08-20 22:22:59 +0200

    Updates for React inclusion

--- src/stories/components.js
+++ src/stories/components.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { storiesOf } from '@storybook/react';
 
 import Alert from '../components/Alert/Alert';
--- src/stories/layout.js
+++ src/stories/layout.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Layouts', module);
+storiesOf('Layout', module);
